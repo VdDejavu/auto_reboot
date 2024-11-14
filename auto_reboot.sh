b3391f5823c833dddab17dade1120ad2cec173dc
VdDejavu <124118152+VdDejavu@users.noreply.github.com>
2024-11-14 17:43:13 +0700

    Update auto_reboot.sh

--- auto_reboot.sh
+++ auto_reboot.sh
@@ -5,7 +5,7 @@ if [[ "$(ping -c 10 8.8.8.8 | grep '100% packet loss')" != "" ]]; then
    echo "DOWN `date +%Y-%m-%dT%H:%M:%S%Z` $((`date +%s`-$TIMESTAMP))";
    /sbin/reboot
   exit 1
-elif [[ "$(ping -c 10 -q 8.8.8.8 | grep -oP '\d+(?=% packet loss)')" != "" ]]; then
+elif [[ "$(ping -c 10 -q 8.8.8.8 | grep -oP '\d+(?=% packet loss)')" != 0 ]]; then
    echo "OFFLINE `date +%Y-%m-%dT%H:%M:%S%Z` $((`date +%s`-$TIMESTAMP))";
    /sbin/reboot
   exit 1
