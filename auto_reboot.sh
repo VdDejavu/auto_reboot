a6f80a7fc1d813c530408a9b95953cae0409132b
VdDejavu <124118152+VdDejavu@users.noreply.github.com>
2024-11-14 08:35:01 +0700

    Update auto_reboot.sh

--- auto_reboot.sh
+++ auto_reboot.sh
@@ -3,11 +3,11 @@
 TIMESTAMP=`date +%s`
 if [[ "$(ping -c 1 8.8.8.8 | grep '100% packet loss' )" != "" ]]; then
    echo "DOWN `date +%Y-%m-%dT%H:%M:%S%Z` $((`date +%s`-$TIMESTAMP))";
-   reboot
+   /sbin/reboot
   exit 1
 elif [[ "$(ping -c 1 8.8.8.8 | grep 'packet loss' | grep -o "1[0-9][0-9]\+%")" != "" ]]; then
    echo "OFFLINE `date +%Y-%m-%dT%H:%M:%S%Z` $((`date +%s`-$TIMESTAMP))";
-   reboot
+   /sbin/reboot
   exit 1
 else
   echo "ONLINE   `date +%Y-%m-%dT%H:%M:%S%Z` $((`date +%s`-$TIMESTAMP))";
