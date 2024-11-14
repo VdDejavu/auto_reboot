80cf2e7b7c81ac3485277ebc33168b93da3f2220
VdDejavu <124118152+VdDejavu@users.noreply.github.com>
2024-11-14 17:59:00 +0700

    Update auto_reboot.sh

--- auto_reboot.sh
+++ auto_reboot.sh
@@ -1,7 +1,7 @@
  #!/bin/bash
 
 TIMESTAMP=`date +%s`
-if [[ "$(ping -c 10 8.8.8.8 | grep '100% packet loss')" != "" ]]; then
+if [[ "$(ping -c 10 8.8.8.8 | grep '100% packet loss')" == 100 ]]; then
    echo "DOWN `date +%Y-%m-%dT%H:%M:%S%Z` $((`date +%s`-$TIMESTAMP))";
    /sbin/reboot
   exit 1
