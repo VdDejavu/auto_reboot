32cd5e8c8cca3f3ddbccd12b9b8bada0864e8b08
VdDejavu <124118152+VdDejavu@users.noreply.github.com>
2024-11-15 10:16:14 +0700

    Update auto_reboot.sh

--- auto_reboot.sh
+++ auto_reboot.sh
@@ -1,11 +1,19 @@
  #!/bin/bash
 
-TIMESTAMP=`date +%s`
-if [[ "$(ping -c 10 8.8.8.8 | grep '100% packet loss')" == 100 ]]; then
-   echo "DOWN `date +%Y-%m-%dT%H:%M:%S%Z` $((`date +%s`-$TIMESTAMP))";
-   /sbin/reboot
-  exit 1
+Internet=192.168.1.1
+Lan=192.168.0.1
+
+DATE=$(date '+%d/%m/%Y %H:%M:%S')
+if [[ "$(ping -c 10 $Internet | grep -oP '\d+(?=% packet loss)')" == 100 ]]; then
+	if [[ "$(ping -c 10 $Lan | grep -oP '\d+(?=% packet loss)')" == 100 ]]; then
+		echo "$DATE Internet: DOWN, Lan: DOWN";
+		echo "$DATE Reboot Now";
+		exit 1
+	else
+	    echo "$DATE Internet: DOWN, Lan: OK";
+	fi
+exit 1
 else
-  echo "ONLINE   `date +%Y-%m-%dT%H:%M:%S%Z` $((`date +%s`-$TIMESTAMP))";
+  echo "$DATE Internet: OK, Lan: OK";
   exit 0
 fi
